f4e6f1cad3c90c8b22f1aa07e0cc7dedcc421c86
tinchomaz <96412664+tinchomaz@users.noreply.github.com>
2022-05-24 18:58:12 -0300

    Add files via upload

---
+++ Paginas/lib/JS/Usuario_Contra.js
@@ -0,0 +1,4 @@
+function ingreso(){
+    if (document.form.usuario.value=="asd" && document.form.contra.value==123)
+    window.location.replace("lib/HTML/index.html")
+else{alert("datos incorrectos")}}

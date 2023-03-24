81461f3f7b0e16fffe11ad122c7fa7b38a25c76d
Martin <juanmartinf7@hotmail.com>
2023-03-23 23:02:32 -0300

    arrancando de 0

--- Paginas/lib/JS/Usuario_Contra.js
+++
@@ -1,4 +0,0 @@
-function ingreso(){
-    if (document.form.usuario.value=="asd" && document.form.contra.value==123)
-    window.location.replace("lib/HTML/index.html")
-else{alert("datos incorrectos")}}
